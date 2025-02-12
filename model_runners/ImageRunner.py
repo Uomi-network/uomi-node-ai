3493ee1235fb12e1eab8598035f9ff788b9805a2
Gregorio Galante <me@gregoriogalante.com>
2025-02-12 15:55:31 +0100

    Remove image save

--- model_runners/ImageRunner.py
+++ model_runners/ImageRunner.py
@@ -8,7 +8,7 @@ class ImageRunner(ModelRunner):
       return self._generate_error("input parameter must be a string")
 
     response = model_manager.run_inference(input)
-    response.save("last_image.jpg")
+    # response.save("ImageRunner_last.jpg")
     response_as_bytes = response.tobytes()
     response_as_hex_string = response_as_bytes.hex()
     return self._generate_output(response_as_hex_string)
