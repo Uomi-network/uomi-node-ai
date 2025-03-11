c140f8bc36be1fb3ba3d2f0b4721faaea7b870b8
Gregorio Galante <me@gregoriogalante.com>
2025-03-11 15:22:12 +0100

    Added service file

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-conda create -n uomi-ai2 python=3.10 -y
-conda activate uomi-ai2
+conda create -n uomi-ai python=3.10 -y
+conda activate uomi-ai
 conda install pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 conda install -c nvidia/label/cuda-12.1.0 cuda-nvcc=12.1 cuda-toolkit=12.1
 conda install numpy=1.24
